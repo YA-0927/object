8467e94e93b42c016edd242ae4aba7c01f85592a
YA-0927 <xox.like.0927@gmail.com>
2021-12-07 12:40:41 +0900

    1206：カプレカ数のアルゴリズム

--- src/self_study/kaprekar/Kaprekar.java
+++ src/self_study/kaprekar/Kaprekar.java
@@ -3,47 +3,54 @@ package self_study.kaprekar;
 import java.util.*;
 
 class Kaprekar {
-    //4桁のカプレカ数を求める
     public static void main(String[] args) {
+        System.out.println("");
         //ランダムで4桁の数字
-        double random =Math.random()*10000;
+        double random = Math.random() * 10000;
         //double型をint型にキャスト
-        int r = (int)random;
-        System.out.println("生成された4桁の数字:" +r);
+        int tempNum = (int) random;
+        System.out.println("生成された4桁の数字:" + tempNum);
 
-        //4桁の数値をそれぞれ分割
-        String str = Integer.toString(r);
-        String[] arrayStr = str.split("");
-        //System.out.println("String型配列"+ Arrays.toString(arrayStr)); //表示して確認
+        //比較のためにkaprekarNumにtempNumを設定
+        int kaprekarNum = tempNum;
 
-        //整数にする
-        int[] arrayInt = new int[arrayStr.length];
-        for (int i = 0; i < arrayInt.length; i++) {
-        }
-        //昇順にする
-        Arrays.sort(arrayStr);
-        System.out.println("最小値配列"+ Arrays.toString(arrayStr));
+        //後判定繰り返し
+        do {
+            tempNum = kaprekarNum;
+            //4桁の数値をそれぞれ分割
+            String str = Integer.toString(tempNum);
+            String[] arrayStr = str.split("");
+            //System.out.println("String型配列"+ Arrays.toString(arrayStr)); //表示して確認
 
-        //最小値としてminに代入
-        String minN = String.join("",arrayStr);
-        System.out.println(minN);
+            //整数にする
+            int[] arrayInt = new int[arrayStr.length];
+            for (int i = 0; i < arrayInt.length; i++) {
+            }
 
-        //最大値としてmaxに代入
-        Collections.reverse(Arrays.asList(arrayStr));
-        System.out.println("最大値配列"+ Arrays.toString(arrayStr));
-        String maxN = String.join("",arrayStr);
-        System.out.println(maxN);
+            //昇順にする
+            Arrays.sort(arrayStr);
+            System.out.println("最小値配列" + Arrays.toString(arrayStr));
 
-        int max = Integer.parseInt(maxN);
-        int min = Integer.parseInt(minN);
+            //最小値としてminに代入
+            String minN = String.join("", arrayStr);
+            System.out.println(minN);
 
-        int tempNum = max - min;
-        int kaprelaNum = tempNum;
+            //最大値としてmaxに代入
+            Collections.reverse(Arrays.asList(arrayStr));
+            System.out.println("最大値配列" + Arrays.toString(arrayStr));
+            String maxN = String.join("", arrayStr);
+            System.out.println(maxN);
 
-        System.out.println(" ");
+            int max = Integer.parseInt(maxN);
+            int min = Integer.parseInt(minN);
 
-        System.out.println("最大値－最小値：" + tempNum);
+            kaprekarNum = max - min;
 
-        //if(Kaprekar)
+            System.out.println("最大値－最小値：" + kaprekarNum);
+
+            System.out.println(" ");
+        }while(kaprekarNum != tempNum);
+
+        System.out.println("∴カプレカ数："+ kaprekarNum );
     }
 }
