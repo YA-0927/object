10640d574e3fb82fd34e352bb59beda86c380167
YA-0927 <xox.like.0927@gmail.com>
2021-12-07 12:46:00 +0900

    1206：カプレカ数のアルゴリズム

--- src/self_study/kaprekar/Kaprekar.java
+++ src/self_study/kaprekar/Kaprekar.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 class Kaprekar {
     public static void main(String[] args) {
-        System.out.println("");
         //ランダムで4桁の数字
         double random = Math.random() * 10000;
         //double型をint型にキャスト
@@ -14,6 +13,8 @@ class Kaprekar {
         //比較のためにkaprekarNumにtempNumを設定
         int kaprekarNum = tempNum;
 
+        System.out.println("");
+
         //後判定繰り返し
         do {
             tempNum = kaprekarNum;
@@ -22,24 +23,19 @@ class Kaprekar {
             String[] arrayStr = str.split("");
             //System.out.println("String型配列"+ Arrays.toString(arrayStr)); //表示して確認
 
-            //整数にする
-            int[] arrayInt = new int[arrayStr.length];
-            for (int i = 0; i < arrayInt.length; i++) {
-            }
-
             //昇順にする
             Arrays.sort(arrayStr);
-            System.out.println("最小値配列" + Arrays.toString(arrayStr));
+            //System.out.println("最小値配列" + Arrays.toString(arrayStr));
 
             //最小値としてminに代入
             String minN = String.join("", arrayStr);
-            System.out.println(minN);
+            System.out.println("最小値" + minN);
 
             //最大値としてmaxに代入
             Collections.reverse(Arrays.asList(arrayStr));
-            System.out.println("最大値配列" + Arrays.toString(arrayStr));
+            //System.out.println("最大値配列" + Arrays.toString(arrayStr));
             String maxN = String.join("", arrayStr);
-            System.out.println(maxN);
+            System.out.println("最大値" + maxN);
 
             int max = Integer.parseInt(maxN);
             int min = Integer.parseInt(minN);
